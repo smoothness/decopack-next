57b580e852d998ce0e0427b0a83f75ed6d05bab7
Mauricio Poveda <mauricio.poveda@gmail.com>
2025-04-01 20:36:28 -0600

    Fix prettier plugin init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,7 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'prettier',
-    'prettier-plugin-tailwindcss',
-  ),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
 ]
 
 export default eslintConfig
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function ensureStartWith(stringToCheck: string, prefix: string): string {
   return stringToCheck.startsWith(prefix)
     ? stringToCheck
